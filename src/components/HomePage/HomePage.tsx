1fe94bf4eaa89b9c795d2fb0ca78567ac1f4f904
VaniaToper <vanjaqwe58@gmail.com>
2021-12-11 00:16:46 +0100

    feat🔨: add tree anim

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -6,15 +6,19 @@ interface IProps {
 }
 
 const HomePage: React.FC<IProps> = () => {
-    return (
-        <div className={s.home}>
-            <div className={s.home__ground__wrapper}>
-                <div className={[s.home__ground_left, s.home__ground].join(' ')}/>
-                <div className={[s.home__ground_right, s.home__ground].join(' ')}/>
-            </div>
-            <Button>Start</Button>
-        </div>
-    );
+  return (
+    <div className={s.home}>
+      <div className={s.home__ground__wrapper}>
+        <div className={[s.home__ground_left, s.home__ground].join(' ')}/>
+        <div className={[s.home__ground_right, s.home__ground].join(' ')}/>
+      </div>
+      <div className={[s.home__tree, s.tree1].join(' ')}/>
+      <div className={[s.home__tree, s.tree2].join(' ')}/>
+      <div className={[s.home__tree, s.tree3].join(' ')}/>
+      <div className={s.home__text}>Help grandma with decorating <br/> the Christmas tree</div>
+      <Button>Start</Button>
+    </div>
+  );
 };
 
 export default HomePage;
